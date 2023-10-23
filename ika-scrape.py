9d874bd8d6e9940074681c0d6b20f66c6d50047a
TheForeigner01 <kovacsbence.se@gmail.com>
2023-10-23 18:54:17 +0200

    GUI added

--- ika-scrape.py
+++ ika-scrape.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.options import Options
 
 from bs4 import BeautifulSoup
 
+import PySimpleGUI as sg
+
 def is_island_tile(tag):
     return tag.name == 'div' and 'islandTile' in tag.get('class', [])
 
@@ -174,13 +176,13 @@ def set_offset_param(offset):
     url = "&offset=" + str(offset)
     return url
 
-def fetch_military_hs(top):
+def fetch_military_hs(top, progressbar):
     full_data = "Position;Player Name;Alliance;Points\n"
 
     print("Fetching data:")
     for i in range(0,top,50):
         print("\tfetching positions "+str(i+1)+"-"+str(i+50)+"...")
-            
+        progressbar.UpdateBar(i+50)
         params=highscore_military_score_param + set_offset_param(i)
         driver.get(highscore_url+params)
             
@@ -191,7 +193,7 @@ def fetch_military_hs(top):
 
     write_to_file(full_data,out_file)
 
-def refresh_local_map():
+def refresh_local_map(progressbar):
     
     islands = [["x" for x in range(1,101)] for y in range(1,101)]
     
@@ -211,6 +213,7 @@ def refresh_local_map():
             
             progress += 1
             print("Scanning Islands",progress*100/25 ,"%\n")
+            progressbar.UpdateBar(progress+1)
             
             for j in vquinto:
                 for i in j:
@@ -226,7 +229,7 @@ def refresh_local_map():
     
     write_to_file(islandstr,"island.txt")
 
-def find_island(miracle_filter="none", tgood_filter="none", x_min_filter = 1, x_max_filter=100, y_min_filter=1, y_max_filter=100, max_cities_filter = 15, min_cities_filter = 1, is_ally = 0):
+def find_island(filters,miracle_select, window, is_ally = 0):
     
     f = open("island.txt","r")
     
@@ -240,13 +243,14 @@ def find_island(miracle_filter="none", tgood_filter="none", x_min_filter = 1, x_
         for i in row:
             if len(i) >1 :
                 i = ast.literal_eval(i)
-                if min_cities_filter <= int(i[5]) <= max_cities_filter:
-                    if i[1] == miracle_filter or miracle_filter == "none":
-                        if i[2] == tgood_filter or tgood_filter == "none":
-                            if x_min_filter <= i[3] <= x_max_filter:
-                                if y_min_filter <= i[4] <= y_max_filter:
+                if filters["citymin_slider"] <= int(i[5]) <= filters["citymax_slider"]:
+                    if i[1] in miracle_select or "none" in miracle_select:
+                        if i[2] in filters["goods_combo"] or "none" in filters["goods_combo"]:
+                            if filters["xmin_slider"] <= i[3] <= filters["xmax_slider"]:
+                                if filters["ymin_slider"] <= i[4] <= filters["ymax_slider"]:
                                     if i[6] >= is_ally:
-                                        print(i[0],i[1],i[2],i[5],sep=", ")
+                                        sg.cprint_set_output_destination(window,'island_res_ml')
+                                        sg.cprint(i[0],i[1],i[2],i[5],sep=", ")
     
 def ms_compare(): #TODO add ms compare feature
     all_csv = []
@@ -281,23 +285,74 @@ options.add_argument("--headless=new")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 
-while True:
-    auth_mode = input("select authentication mode:\n(1) Cookie\n(2) Login (email + password) (BETA)\n___________________\n:")
-    if auth_mode == "1":
-        ika_cookie = input("Please provide a valid 'ikariam' cookie:")
-        print("Authenticating...")
+
+modeselect_layout  = [  
+    [sg.Text('Select authentication mode:')],
+    [sg.Radio(f'Cookie', 1)],
+    [sg.Radio(f'Login with email and password (BETA)', 1)    ],
+    [sg.Button('Select'), sg.Button('Cancel')]  
+]
+
+cookie_layout = [
+    [sg.Text("Please provide a valid 'ikariam' cookie:"), sg.InputText()],
+    [sg.Button('Ok'), sg.Button('Cancel')]  
+]
+
+email_layout = [
+    [sg.Text("Email:"), sg.InputText()],
+    [sg.Text("Password:"), sg.InputText()],
+    [sg.Button('Ok'), sg.Button('Cancel')]  
+]
+
+w84auth_layout = [
+    [sg.Text("Authenticating, Please wait...")]
+]
+
+login_layout = [
+    [sg.Column(modeselect_layout,visible=True, key="login"),
+     sg.Column(cookie_layout,visible=False, key="cookie"),
+     sg.Column(email_layout,visible=False, key="email"),
+     sg.Column(w84auth_layout,visible=False, key="w84auth")]
+]
+
+
+login_window = sg.Window("Ika-Scrape",login_layout)
+auth_mode = 0
+active_layout = "login"
+while True:  #login loop
+    
+    event, values = login_window.read()
+    if event == sg.WIN_CLOSED or 'Cancel' in event: # if user closes window or clicks cancel
+        break
+    elif event == "Select":
+        login_window[f'login'].update(visible=False)
+        if values[0]:
+            active_layout = "cookie"
+            login_window[f'cookie'].update(visible=True)
+            continue
+        elif values[1]:
+            active_layout = "email"
+            login_window[f'email'].update(visible=True)
+            continue
+    elif "Ok" in event and active_layout == "cookie":
+        
+        active_layout = "w84auth"
+        login_window[f'cookie'].update(visible=False)
+        login_window[f'w84auth'].update(visible=True)
+        
+        login_window.refresh()
+        ika_cookie = values[2]
         driver.get(url)
         driver.add_cookie({"name": "ikariam", "value": ika_cookie})
-        driver.refresh()
-        
-        
         break
-    elif auth_mode == "2":
-       
-        
+    elif "Ok" in event and active_layout == "email":
+        active_layout = "w84auth"
+        login_window[f'email'].update(visible=False)
+        login_window[f'w84auth'].update(visible=True)
+        event, values = login_window.read(10)
         driver.get(lobby_url)
         
-        time.sleep(1)
+        time.sleep(2)
         
         login_tab = driver.find_element(By.XPATH, "//ul[@class='tabsList']/li[text()='Log in']")
         register_tab = driver.find_element(By.XPATH, "//ul[@class='tabsList']/li[text()='Register']")
@@ -307,114 +362,186 @@ while True:
         email_field = logintab.find_element(By.NAME,"email")
         pw_field = logintab.find_element(By.NAME,"password")
         
-        email_in = input("Email:")
-        password_in = input("Password:")
+        time.sleep(1)
+        
+        email_in = values[3]
+        password_in = values[4]
         
         email_field.send_keys(email_in)
         pw_field.send_keys(password_in)
-        time.sleep(1)
+        time.sleep(3)
         
-        driver.find_element(By.CLASS_NAME, "button-primary").click()
-        time.sleep(1)
-        
+        driver.find_element(By.CLASS_NAME, "button-lg").click()
+        time.sleep(3)
         driver.get(lobby_url+"/accounts")
         time.sleep(1)
         
         pangaia5 = driver.find_element(By.XPATH, "//div[contains(text(), 'Pangaia 5')]")
-        time.sleep(1)
         pangaia5.click() 
         time.sleep(1)
         driver.find_element(By.CLASS_NAME, "btn-primary").click()
-        
-        print("Authenticating...")
         break
-    else:
-        print("unknown command")
+    
+    #print(event, values)
 
+if active_layout == "email":
+    time.sleep(5)
+    
+driver.get(world_view_url)
+logged_in = driver.find_element(By.CLASS_NAME, "noViewParameters").text
 
-while True:
-    print("\n\nPlease choose from the following options what you would like to do.")
+login_window.close()
+
+xcoord_min = 1
+xcoord_max = 100
+ycoord_min = 1
+ycoord_max = 100
+citymax = 17
+citymin = 0
+is_ally = 0
+
+toggle_btn_off = b'iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAABmJLR0QA/wD/AP+gvaeTAAAED0lEQVRYCe1WTWwbRRR+M/vnv9hO7BjHpElMKSlpqBp6gRNHxAFVcKM3qgohQSqoqhQ45YAILUUVDRxAor2VAweohMSBG5ciodJUSVqa/iikaePEP4nj2Ovdnd1l3qqJksZGXscVPaylt7Oe/d6bb9/svO8BeD8vA14GvAx4GXiiM0DqsXv3xBcJU5IO+RXpLQvs5yzTijBmhurh3cyLorBGBVokQG9qVe0HgwiXLowdy9aKsY3g8PA5xYiQEUrsk93JTtjd1x3siIZBkSWQudUK4nZO1w3QuOWXV+HuP/fL85klAJuMCUX7zPj4MW1zvC0Ej4yMp/w++K2rM9b70sHBYCjo34x9bPelsgp/XJksZ7KFuwZjr3732YcL64ttEDw6cq5bVuCvgy/sje7rT0sI8PtkSHSEIRIKgCQKOAUGM6G4VoGlwiqoVd2Za9Vl8u87bGJqpqBqZOj86eEHGNch+M7otwHJNq4NDexJD+59RiCEQG8qzslFgN8ibpvZNsBifgXmFvJg459tiOYmOElzYvr2bbmkD509e1ylGEZk1Y+Ssfan18n1p7vgqVh9cuiDxJPxKPT3dfGXcN4Tp3dsg/27hUQs0qMGpRMYjLz38dcxS7Dm3nztlUAb38p0d4JnLozPGrbFfBFm79c8hA3H2AxcXSvDz7/+XtZE1kMN23hjV7LTRnKBh9/cZnAj94mOCOD32gi2EUw4FIRUMm6LGhyiik86nO5NBdGRpxYH14bbjYfJteN/OKR7UiFZVg5T27QHYu0RBxoONV9W8KQ7QVp0iXdE8fANUGZa0QAvfhhXlkQcmjJZbt631oIBnwKmacYoEJvwiuFgWncWnXAtuVBBEAoVVXWCaQZzxmYuut68b631KmoVBEHMUUrJjQLXRAQVSxUcmrKVHfjWWjC3XOT1FW5QrWpc5IJdQhDKVzOigEqS5dKHMVplnNOqrmsXqUSkn+YzWaHE9RW1FeXL7SKZXBFUrXW6jIV6YTEvMAUu0W/G3kcxPXP5ylQZs4fa6marcWvvZfJu36kuHjlc/nMSuXz+/ejxgqPFpuQ/xVude9eu39Jxu27OLvBGoMjrUN04zrNMbgVmOBZ96iPdPZmYntH5Ls76KuxL9NyoLA/brav7n382emDfHqeooXyhQmARVhSnAwNNMx5bu3V1+habun5nWdXhwJZ2C5mirTesyUR738sv7g88UQ0rEkTDlp+1wwe8Pf0klegUenYlgyg7bby75jUTITs2rhCAXXQ2vwxz84vlB0tZ0wL4NEcLX/04OrrltG1s8aOrHhk51SaK0us+n/K2xexBxljcsm1n6x/Fuv1PCWGiKOaoQCY1Vb9gWPov50+fdEqd21ge3suAlwEvA14G/ucM/AuppqNllLGPKwAAAABJRU5ErkJggg=='
+toggle_btn_on = b'iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAABmJLR0QA/wD/AP+gvaeTAAAD+UlEQVRYCe1XzW8bVRCffbvrtbP+2NhOD7GzLm1VoZaPhvwDnKBUKlVyqAQ3/gAkDlWgPeVQEUCtEOIP4AaHSI0CqBWCQyXOdQuRaEFOk3g3IMWO46+tvZ+PeZs6apq4ipON1MNafrvreTPzfvub92bGAOEnZCBkIGQgZOClZoDrh25y5pdjruleEiX+A+rCaQo05bpuvJ/+IHJCSJtwpAHA/e269g8W5RbuzF6o7OVjF8D3Pr4tSSkyjcqfptPDMDKSleW4DKIggIAD5Yf+Oo4DNg6jbUBlvWLUNutAwZu1GnDjzrcXzGcX2AHw/emFUV6Sfk0pqcKpEydkKSo9q3tkz91uF5aWlo1Gs/mYc+i7tz4//19vsW2AU9O381TiioVCQcnlRsWeQhD3bJyH1/MiFLICyBHiuzQsD1arDvypW7DR9nzZmq47q2W95prm+I9fXfqXCX2AF2d+GhI98Y8xVX0lnxvl2UQQg0csb78ag3NjEeD8lXZ7pRTgftmCu4864OGzrq+5ZU0rCa3m+NzXlzvoAoB3+M+SyWQuaHBTEzKMq/3BMbgM+FuFCDBd9kK5XI5PJBKqLSev+POTV29lKB8rT0yMD0WjUSYLZLxzNgZvIHODOHuATP72Vwc6nQ4Uiw8MUeBU4nHS5HA6TYMEl02wPRcZBJuv+ya+UCZOIBaLwfCwQi1Mc4QXhA+PjWRkXyOgC1uIhW5Qd8yG2TK7kSweLcRGKKVnMNExWWBDTQsH9qVmtmzjiThQDs4Qz/OUSGTwcLwIQTLW58i+yOjpXDLqn1tgmDzXzRCk9eDenjo9yhvBmlizrB3V5dDrNTuY0A7opdndStqmaQLPC1WCGfShYRgHdLe32UrV3ntiH9LliuNrsToNlD4kruN8v75eafnSgC6Luo2+B3fGKskilj5muV6pNhk2Qqg5v7lZ51nBZhNBjGrbxfI1+La5t2JCzfD8RF1HTBGJXyDzs1MblONulEqPDVYXgwDIfNx91IUVbAbY837GMur+/k/XZ75UWmJ77ou5mfM1/0x7vP1ls9XQdF2z9uNsPzosXPNFA5m0/EX72TBSiqsWzN8z/GZB08pWq9VeEZ+0bjKb7RTD2i1P4u6r+bwypo5tZUumEcDAmuC3W8ezIqSGfE6g/sTd1W5p5bKjaWubrmWd29Fu9TD0GlYlmTx+8tTJoZeqYe2BZC1/JEU+wQR5TVEUPptJy3Fs+Vkzgf8lemqHumP1AnYoMZSwsVEz6o26i/G9Lgitb+ZmLu/YZtshfn5FZDPBCcJFQRQ+8ih9DctOFvdLIKHH6uUQnq9yhFu0bec7znZ+xpAGmuqef5/wd8hAyEDIQMjAETHwP7nQl2WnYk4yAAAAAElFTkSuQmCC'
+
+main_menu_layout = [
+    [sg.Text("You are logged in as "+logged_in)],
+    [sg.Text("")],
+    [sg.Text("Please choose from the following options what you would like to do.")],
+    [sg.Radio(f"Fetch top 2000 Military Scores", 1)],
+    [sg.Radio(f'Refresh local map', 1)    ],
+    [sg.Radio(f'Search Island', 1)    ],
+    [sg.Button('Ok'), sg.Button('Cancel')] 
+]
+
+fetch_ms_layout = [
+    [sg.Text("Fetching Military scores...")],
+    [sg.ProgressBar(2000, orientation='h', size=(20,20), key='fetching_progress')]
+]
+
+refresh_map_layout = [
+    [sg.Text("Refreshing local map...")],
+    [sg.ProgressBar(25, orientation='h', size=(20,20), key='map_progress')]
+]
+
+trade_goods = [
+    "Wine",
+    "Marble",
+    "Crystal Glass",
+    "Sulphur",
+    "none"
+]
+
+miracles = [
+    "Hephaistos' Forge",
+    "Hades' Holy Grove",
+    "Demeter's Gardens",
+    "Temple of Athene",
+    "Temple of Hermes",
+    "Ares' Stronghold",
+    "Temple of Poseidon",
+    "Colossus"
+]
+
+island_search_layout = [
+    [sg.Combo(trade_goods, default_value="none",enable_events=True,s=(20,5),k='goods_combo')],
+    [sg.Checkbox(miracles[i],key=miracles[i]) for i in range(0,2)],
+    [sg.Checkbox(miracles[i],key=miracles[i]) for i in range(2,5)],
+    [sg.Checkbox(miracles[i],key=miracles[i]) for i in range(5,8)],
+    [sg.Text("X coordinate min and max"), sg.Slider((1,xcoord_max),default_value=xcoord_min, orientation='h', s=(10,15),k='xmin_slider'), sg.Slider((xcoord_min,100),default_value=xcoord_max, orientation='h', s=(10,15),k='xmax_slider')],
+    [sg.Text("Y coordinate min and max"), sg.Slider((1,ycoord_max),default_value=ycoord_min, orientation='h', s=(10,15),k='ymin_slider'), sg.Slider((ycoord_min,100),default_value=ycoord_max, orientation='h', s=(10,15),k='ymax_slider')],
+    [sg.Text("Minimum and maximum number of cities"), sg.Slider((0,citymax),default_value=citymin, orientation='h', s=(10,15),k='citymin_slider'),sg.Slider((citymin,17),default_value=citymax, orientation='h', s=(10,15),k='citymax_slider')],
+    [sg.Text("Show only ally cities"), sg.Button(image_data=toggle_btn_off, key='-TOGGLE-GRAPHIC-', button_color=(sg.theme_background_color(), sg.theme_background_color()),border_width=0, metadata=False)],
+    [sg.Button('Ok'), sg.Button('Back')] 
+]
+
+island_results_layout = [
+    [sg.Text("Island Name, Miracle, Trade Good, Number of cities")],
+    [sg.Multiline(size=(50,15),k='island_res_ml')]
+]
+
+main_layout = [
+    [sg.Column(main_menu_layout,visible=True,key='main_menu'),
+     sg.Column(fetch_ms_layout,visible=False, key='fetch_ms'),
+     sg.Column(refresh_map_layout, visible=False, key='refresh_map'),
+     sg.Column(island_search_layout, visible=False, key='island_search'),
+     sg.Column(island_results_layout, visible=False, key='island_results'),
+     ]
+]
+
+main_window = sg.Window("Ika-Scrape",main_layout)
+
+while True: #main loop
     
-    options = [
-        "(1) Fetch top 2000 Military Scores\n",
-        "(2) Refresh local map\n",
-        "(3) Search Island\n",
-        "(q - quit) Quit program\n"
-    ]
-    
-    for o in options:
-        print(o)
-    print("___________________\n:")
-    
-    
-    select = input()
-    if select == "1":
-        #get military scores
-        fetch_military_hs(2000)
+    event, values = main_window.read()
+    #print(event, values)
+    if event == sg.WIN_CLOSED or 'Cancel' in event: # if user closes window or clicks cancel
+        break
+    elif event == "Ok" and values[0]: #fetch ms
+        main_window[f'main_menu'].update(visible=False)
+        main_window[f'fetch_ms'].update(visible=True)
+        main_window.read(10)
+        fetch_military_hs(2000,main_window["fetching_progress"])
+        main_window[f'fetch_ms'].update(visible=False)
+        main_window[f'main_menu'].update(visible=True)
+        main_window.read(10)
         
-    elif select == "2":
-        refresh_local_map()
-    
-    elif select == "3":
-        miracle_filter = "none"
-        tradegood_filter = "none"
-        x_filter = (1,100)
-        y_filter = (1,100)
-        cities_filter = (0,17)
-        
-        miracles = [
-            "Hephaistos' Forge",
-            "Hades' Holy Grove",
-            "Demeter's Gardens",
-            "Temple of Athene",
-            "Temple of Hermes",
-            "Ares' Stronghold",
-            "Temple of Poseidon",
-            "Colossus",
-            "none"
-        ]
-        
-        trade_goods = [
-            "Wine",
-            "Marble",
-            "Crystal Glass",
-            "Sulphur",
-            "none"
-        ]
-        
-        print("What miracle would you like to search for?")
-        for idx, miracle in enumerate(miracles):
-            print("("+str(idx+1)+") "+miracle)
-        miracle_select = int(input(":"))
+    elif event == "Ok" and values[1]: #refresh map
+        main_window[f'main_menu'].update(visible=False)
+        main_window[f'refresh_map'].update(visible=True)
+        main_window.read(10)
+        refresh_local_map(main_window['map_progress'])
+        main_window[f'refresh_map'].update(visible=False)
+        main_window[f'main_menu'].update(visible=True)
+        main_window.read(10)
+    elif event == "Ok" and values[2]: #search island
+        main_window[f'main_menu'].update(visible=False)
+        main_window[f'island_search'].update(visible=True)
+        main_window[f'island_results'].update(visible=True)
+        while event != 'Back':
             
-        print("What luxury good would you like to search for?")
-        for idx, good in enumerate(trade_goods):
-            print("("+str(idx+1)+") "+good)
-        good_select = int(input(":"))
+            event, values = main_window.read()
+            
+            xcoord_min = values['xmin_slider']
+            xcoord_max = values['xmax_slider']
+            ycoord_min = values['ymin_slider']
+            ycoord_max = values['ymax_slider']
+            citymin = values['citymin_slider']
+            citymax = values['citymax_slider']
+            main_window.refresh()
+            
+            if 'Ok' in event:
+                ml = main_window.find_element("island_res_ml")
+                miracle_select = []
+                #print(event,values)
+                for m in miracles:
+                    if values[m]:
+                        miracle_select.append(m)
+                        #print(m)
+                if(len(miracle_select) == 0):
+                    miracle_select.append("none")
+                    
+                good_select = values['goods_combo']
+                
+                find_island(values,miracle_select,main_window,is_ally=is_ally)
+                ml.print("--------------------------------------------------")
+            elif event == '-TOGGLE-GRAPHIC-':  # if the graphical button that changes images
+                main_window['-TOGGLE-GRAPHIC-'].metadata = not main_window['-TOGGLE-GRAPHIC-'].metadata
+                main_window['-TOGGLE-GRAPHIC-'].update(image_data=toggle_btn_on if main_window['-TOGGLE-GRAPHIC-'].metadata else toggle_btn_off)
+                if is_ally:
+                    is_ally = 0
+                else: is_ally = 1
+                #print(is_ally)
+                
+        main_window[f'island_search'].update(visible=False)
+        main_window[f'island_results'].update(visible=False)
+        main_window[f'main_menu'].update(visible=True)
+        main_window.read(10)
         
-        
-        xmin = int(input("minimum x coordinate:"))
-        xmax = int(input("maximum x coordinate:"))
-        ymin = int(input("minimum y coordinate:"))
-        ymax = int(input("maximum y coordinate:"))
-        citymin = int(input("minimum number of cities:"))
-        citymax = int(input("maximum number of cities:"))
-        is_ally = int(input("list only ally islands?\n(0) no\n(1) yes\n:"))
-        
-        find_island(miracles[miracle_select-1],trade_goods[good_select-1],xmin,xmax,ymin,ymax,citymax,citymin,is_ally)
-        
+     
+main_window.close()  
+driver.quit()
+    
 
     
-    
-    elif select == "quit" or select =="q":
-        print("Quitting program...")
-        driver.quit()
-        exit()
-    
-    else:
-        print("unknown command.")
-    
-    
-
 
 
