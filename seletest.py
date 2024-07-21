591a8797b4e8901dd841c8da7356894f1d5f12c0
qwe asd <covax@server1.zombie-pirates.website>
2024-07-21 23:23:23 +0000

    server changes

--- seletest.py
+++ seletest.py
@@ -10,15 +10,34 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 zps = "https://zombie-pirates.website"
 ika_lobby_url = "https://lobby.ikariam.gameforge.com/"
-driver.get(ika_lobby_url)
-#print(driver.page_source)
+ika_cityview = "https://s305-en.ikariam.gameforge.com/?view=city"
+cookie = "156929_19b25e7f4aa8f58e6937de53654e3fa5"
+f=open("cookie.txt")
+cookie = f.readline()
+print("cookie:"+cookie, type(cookie))
+f.close()
 
+
+driver.get(ika_cityview)
+driver.add_cookie({"name":"ikariam","value":cookie})
+driver.get(ika_cityview)
+
+#print(driver.page_source)
+#time.sleep(10)
 DISCONNECTED_MSG = 'Unable to evaluate script: no such window: target window already closed\nfrom unknown error: web view not found\n'
 
-
+''''''
 while True:
     logdict = driver.get_log("driver")
     if len(logdict) > 0:
         if logdict[0]["message"] == DISCONNECTED_MSG:
             break
-    time.sleep(1)
+    new_cookie = driver.get_cookie("ikariam")["value"]
+    f = open("cookie.txt","w")
+    #print(new_cookie)
+    if type(new_cookie) == type("asd"):	
+    	f.write(new_cookie)
+    f.close()
+    time.sleep(5)
+''''''
+ 
--- test.py
+++ test.py
@@ -1,19 +1,74 @@
 import requests
 import json
+import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import sys,time,pytz
 
-msg = "asd"
+def getGeneralViewRow(inner_html):
+    soup = BeautifulSoup(inner_html, 'html.parser')
 
-webhook_url = "https://discord.com/api/webhooks/1260238289743511644/cXYGPvAeZVhjmGlOQ8txKTVkgABwPcYV4Bre1YlSeM147qfeSq9r4kcxV0HPWd1-IsNO"
+    cells = soup.findAll("td")
+    rowString = "| "
+    for c in cells:
+        rowString = rowString + str(c.getText(strip=True)).strip().replace("  ","") + " | "
+
+    print(rowString)
+    return rowString
+
+world_view_url = "https://s305-en.ikariam.gameforge.com/?view=worldmap_iso"
+city_view_url = "https://s305-en.ikariam.gameforge.com/?view=city"
+if len(sys.argv) > 1:
+    ika_cookie = sys.argv[1]
+else:
+    ika_cookie = "156929_1bbc7871d9f8eca5cce97f185780a2e5"
+
+options = Options()
+options.add_argument("--headless=new")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+driver.get(world_view_url)
+driver.add_cookie({"name": "ikariam", "value": ika_cookie})
+
+
+generalViewStr = ""
+cityIdStr = "133455"
+embassyPosStr = "9"
+asd = "https://discord.com/api/webhooks/1264720303918026763/COT-4RFFU2_xdb6LZk1q7tUAMKSp_-0gP_BNYhfv2NALPHVrpHNaj0eyjY_DGSxE33g4"
+webhook_url = asd#"https://discord.com/api/webhooks/1260238289743511644/cXYGPvAeZVhjmGlOQ8txKTVkgABwPcYV4Bre1YlSeM147qfeSq9r4kcxV0HPWd1-IsNO"
 params = "?wait=true"
-data = {
-            "content": msg
-        }
 
-response = requests.post(webhook_url,json=data).json()
+def refreshGeneralViewStr(driver):
+    res = ""
+    driver.get(city_view_url.replace("city", f"embassyGeneralAttacksToAlly&cityId={cityIdStr}&position={embassyPosStr}&activeTab=tabEmbassy"))
+    driver.find_element(By.ID, "embassyGeneralAttacksToAlly")
+    embassyTable = driver.find_element(By.CLASS_NAME, "embassyTable").find_elements(By.TAG_NAME, "tr")
+    for idx, row in enumerate(embassyTable):
+        if idx > 0:
+            res += getGeneralViewRow(row.get_attribute("innerHTML")) + "\n"
+    return res
 
-message_id = json.loads(response)["id"]
-print(message_id)
 data = {
-            "content": "qwe"
-        }
-requests.patch(webhook_url+"/messages/"+message_id+params,json=data)
+                "content": "General view"
+            }
+
+response = requests.post(webhook_url+params,json=data)
+#print(response.text)
+message_id = json.loads(response.text)["id"]
+#print(message_id)
+driver.get(city_view_url)
+#time.sleep(10)
+run = True
+while run:
+    generalViewStr = refreshGeneralViewStr(driver)
+    currentTime = datetime.datetime.now(tz=pytz.timezone("Europe/Budapest")).strftime("%Y-%m-%d %H:%M:%S")
+    print(currentTime)
+    data = {
+                "content": "GENERAL VIEW - Attacks on Alliance\t("+currentTime+")\n"+"```"+generalViewStr+"```"
+            }
+    
+    requests.patch(webhook_url+"/messages/"+message_id+params,json=data)
+
+    time.sleep(37)
